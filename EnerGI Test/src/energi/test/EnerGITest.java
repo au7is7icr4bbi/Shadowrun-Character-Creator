c292655d3de8a31f109bbe99cff9ce586fe20d29
Andrew Pigram <amp942@uowmail.edu.au>
2011-12-11 11:23:02 +1100

    Added Test game program as an integration test of EnerGI

---
+++ EnerGI Test/src/energi/test/EnerGITest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package energi.test;
+
+/**
+ * Main class for the integration test
+ * @author andrew
+ */
+public class EnerGITest 
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		TestGame newGame = new TestGame();
+		newGame.run();
+	}
+}
